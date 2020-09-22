1a4440f46b1858fee27f202ed9707e1d49bffbf6
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-09-22 13:47:59 +0200

    Initial commit

---
+++ src/main/java/DatabaseProject.java
@@ -0,0 +1,11 @@
+import processing.core.PApplet;
+
+public class DatabaseProject extends PApplet {
+
+    public static void main(String[] args ) {
+        PApplet.main("DatabaseProject");
+    }
+
+
+
+}
