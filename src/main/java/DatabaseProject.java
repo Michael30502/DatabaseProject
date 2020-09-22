99f456bbaf01d825cfada119aea799fab71f24a2
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-09-22 15:31:24 +0200

    Bruh

--- src/main/java/DatabaseProject.java
+++ src/main/java/DatabaseProject.java
@@ -1,10 +1,25 @@
 import processing.core.PApplet;
-
+import java.sql.*;
+import javax.crypto.*;
+import java.security.*;
+import java.util.Base64;
 public class DatabaseProject extends PApplet {
 
     public static void main(String[] args ) {
         PApplet.main("DatabaseProject");
+        String databaseURL = "jdbc:ucanaccess://src//main//resources//DatabaseUsers.accdb";
+
+try{
+Connection connection = DriverManager.getConnection(databaseURL);
+System.out.println("Bruh");
+
+connection.close();
+} catch (Exception e) {
+    e.printStackTrace();
+}
     }
+
+
 
 
 
