532ce977c0f72d4759f4aaf0450534a00a577f16
MarcoBatman <Marco@terrida.dk>
2020-09-25 15:19:45 +0200

    pushing

--- src/main/java/DatabaseProject.java
+++ src/main/java/DatabaseProject.java
@@ -5,6 +5,10 @@ import java.security.*;
 import java.util.Base64;
 public class DatabaseProject extends PApplet {
 Button but = new Button(this,100,100,200,200);
+InputField username = new InputField(this,400,400,200,50,"Username");
+    InputField password = new InputField(this,400,600,200,50,"Password");
+    boolean usernameCheck, passwordCheck;
+    String inputStringU,inputStringP;
     public static void main(String[] args ) {
         PApplet.main("DatabaseProject");
         String databaseURL = "jdbc:ucanaccess://src//main//resources//DatabaseUsers.accdb";
@@ -27,16 +31,33 @@ connection.close();
 
     @Override
     public void draw() {
-        but.draw();
+     //   but.draw();
+        username.display();
+        password.display();
     }
 
     @Override
     public void mousePressed() {
       //  System.out.println(but.registerClick(mousePressed));
+        usernameCheck=false;
+        passwordCheck=false;
+        usernameCheck=username.mouseCollision(mouseX,mouseY);
+        passwordCheck=password.mouseCollision(mouseX,mouseY);
     }
 
     @Override
     public void keyTyped() {
-        
+    if(usernameCheck==true)
+            inputStringU=username.input(true,key);
+        if(passwordCheck==true)
+            inputStringP=password.input(true,key);
+    }
+
+    @Override
+    public void keyReleased() {
+        if(usernameCheck==true)
+            inputStringU=username.input(false,key);
+        if(passwordCheck==true)
+            inputStringP=password.input(false,key);
     }
 }
---
+++ src/main/java/InputField.java
@@ -0,0 +1,66 @@
+import processing.core.PApplet;
+
+
+public class InputField {
+
+    String inputString ="";
+    PApplet p;
+    int x,y,w,h;
+    String type;
+
+
+    InputField(PApplet p,int x,int y,int w,int h,String type){
+
+        this.p=p;
+
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        this.type = type;
+    }
+
+    String input(boolean check,char key) {
+
+        if (check) {
+
+            if (key != '\b' && key != p.CODED) {
+                if((type.equals("Username")&&inputString.length()<29)||(type.equals("Password")&&inputString.length()<4)) {
+                    inputString += key;
+
+                    inputString = inputString.replaceAll("[^\\p{javaWhitespace}\\p{Alnum}(-)]", "");
+                    inputString = inputString.toUpperCase();
+
+                }
+            } else {
+                if (inputString.length() >= 1)
+                    inputString = inputString.substring(0, inputString.length() - 1);
+
+            }
+        }
+        return inputString;
+    }
+    boolean mouseCollision ( int mX, int mY){
+//Check om mouseX og mouseY, er på inputfeltet
+        if (((mX < x + w ) && ( mX > x)))
+            if(((mY < y + h ) && ( mY > y)))
+                return true;
+
+        return false;
+    }
+
+
+    void display () {
+        p.fill(255);
+        p.rect(x,y,w,h);
+        p.fill(0);
+        p.text(inputString, x+5, y+h/2+7);
+        if(type.equals("Username"))
+            p.text("Username",x-100,y+h/2+7);
+        if(type.equals("Password"))
+            p.text("Password", x - 100, y + h / 2 + 7);
+
+
+    }
+
+}
