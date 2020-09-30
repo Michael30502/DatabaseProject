aa58330a6a753569350d9201a69d8d5a80b91783
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-09-30 11:03:12 +0200

    Cool

--- src/main/java/DatabaseProject.java
+++ src/main/java/DatabaseProject.java
@@ -5,19 +5,31 @@ import java.security.*;
 import java.util.Base64;
 public class DatabaseProject extends PApplet {
 
+
     public static void main(String[] args ) {
         PApplet.main("DatabaseProject");
-        String databaseURL = "jdbc:ucanaccess://src//main//resources//DatabaseUsers.accdb";
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:ucanaccess://src//main//resources//DatabaseUsers.accdb");
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("SELECT [Username] FROM [Users]");
+            ResultSet rp = s.executeQuery("SELECT [Password] FROM [Users]");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                rp.next();
 
-try{
-Connection connection = DriverManager.getConnection(databaseURL);
-System.out.println("Bruh");
+                System.out.println(rp.getString(1));
+                System.out.println("");
 
-connection.close();
-} catch (Exception e) {
-    e.printStackTrace();
-}
+            }
+
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+
+
     }
+
 
 
 
