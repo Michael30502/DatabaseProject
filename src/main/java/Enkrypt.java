8f684edfcc09d7a64e3a0d1ec473006d75773c76
mathias124 <54176496+mathias124@users.noreply.github.com>
2020-10-02 16:20:37 +0200

    mere sikkert?

--- src/main/java/Enkrypt.java
+++ src/main/java/Enkrypt.java
@@ -13,13 +13,13 @@ public class Enkrypt {
         // Her bruges Hexdigits det vil sige hexdecimaler som går fra 1-16 som går fra 0-9 og A-F.
         char hexdecimaler[] = {
                 '0', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', 'A', 'B', 'C', 'D', 'E','F'
+                '8', '9', 'A', 'B', 'C', 'D', 'E',
         }//Skal lukke char'en.
                 ;
         StringBuffer henter = new StringBuffer();//En stringfield
         for (int j = 0; j < k.length; j++) {
-            henter.append(hexdecimaler[(k[j] >> 5) & 0x0f]);
-            henter.append(hexdecimaler[k[j] & 0x0f]);
+            henter.append(hexdecimaler[(k[j] >> 5) & 0x0d]);
+            henter.append(hexdecimaler[k[j] & 0x0d]);
         }
         return henter.toString();
 
